63a706cd9fb820e6734e7d5b0ab4767659385649
Finn <36964028+FinnRules@users.noreply.github.com>
2018-03-23 15:10:31 -0400

    Update ppa.js

--- ppa.js
+++ ppa.js
@@ -10,10 +10,38 @@ var prefix = "!";
 var msgContents;
 var playerList = [];
 var playerMoney = [];
+var commandList = ["!ping", "!enter", "!reset"];
 function random(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;//can be min but not max
+}
+//7s and 12s
+function gamble(bet){
+ if(bet>=playerMoney[msg.author.username]&&playerList[msg.author.username]==true){
+  var die1 = random(1,7);
+       var die2 = random(1,7);
+       var dieTotal = die1 + die2;
+       var prevPlayMoney = playMoney[msg.author.username];
+       msg.channel.send(`Die 1 result: ${die1}
+Die 2 result: ${die2}
+Total: ${dieTotal}`) 
+      if (dieTotal == 7){
+        msg.channel.send(`Your bet of ${bet} has been added to your balance!`)
+        playerMoney[msg.author.username] += bet;
+      } else if (dieTotal == 12){
+        msg.channel.send(`Two times your bet of ${bet} won, adding ${bet*2}`)
+        playerMoney[msg.author.username] += bet*2;
+      } else {
+        msg.channel.send(`You lost, your bet of ${bet} has been subtracted!`)
+        playerMoney[msg.author.username] -= bet; 
+      }
+      msg.channel.send(`Your previous balance of ${prevPlayerBalance[msg.author.username]} is now ${playerMoney[msg.author.username]}`)
+   } else if (playerList[msg.author.username==true]){
+   msg.channel.send(`Your balance of ${playerMoney[msg.author.username]} is not high enough for your bet of ${bet} (Difference of ${(bet-playMoney[msg.author.username])})`)
+    } else {
+    msg.channel.send(`Player not found`)
+    }
 }
 //parser
 client.on('message', msg => {
@@ -39,19 +67,29 @@ client.on('message', msg => {
        }
      break;
      case "!bet1":
-       var die1 = random(1,7);
-       var die2 = random(1,7);
-       var dieTotal = die1 + die2;
-       msg.channel.send(`Die 1 result: ${die1}
-Die 2 result: ${die2}
-Total: ${dieTotal}`) 
-      if (dieTotal == 7){
-        playerMoney[msg.author.username] ++;
-      } else if (dieTotal == 12){
-        playerMoney[msg.author.username] += 2;
-      } else {
-        playerMoney[msg.author.username] --; 
-      }
+      gamble(1);
+     break;
+     case "!bet5":
+      gamble(5);
+     break;
+     case "!bet10":
+      gamble(10);
+     break;
+     case "!bet20":
+      gamble(20);
+     break;
+     case "!bet50":
+      gamble(50);
+     break;
+     case "!bet100":
+      gamble(100);
+     break;
+     case "!betthefarm":
+      gamble(playerMoney[msg.author.username]);
+     break;
+     case "!balance":
+      msg.channel.send(`Balance for ${msg.author.username} is ${playerMoney[msg.author.username]}`)
+     break;
        
        
        //not done yet lol
