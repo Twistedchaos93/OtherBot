445d26c1f10f698ebd75ac72dace52bf07bceb5d
Finn <36964028+FinnRules@users.noreply.github.com>
2018-03-23 13:05:10 -0400

    Update ppa.js

--- ppa.js
+++ ppa.js
@@ -10,6 +10,12 @@ var prefix = "!";
 var msgContents;
 var playerList = [];
 var playerMoney = [];
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;//can be min but not max
+}
+//parser
 client.on('message', msg => {
   if (msg.content.startsWith(prefix)){
    switch(msgContents){
@@ -18,17 +24,28 @@ client.on('message', msg => {
      break;
      case "!enter":       
        if (playerList[msg.author.username] != true){
-       playerMoney = [msg.author.username] = 0.0;
+       playerMoney = [msg.author.username] = 5.0;
        }
        playerList[msg.author.username] = true;
-       msg.channel.send(`You have joined! Balance ${playerMoney[msg.author.username}`)
+       msg.channel.send(`You have joined! Balance ${playerMoney[msg.author.username]}`)
      break;
-     //case "!
+     case "!reset":
+       if (msg.author.username == "FinnRules"){
+        playerList = [];
+        playerMoney = [];
+        msg.channel.send(`All players removed by ${msg.author.username}`)
+       } else {
+        msg.channel.send(`${msg.author.send} is not recognised as a user who reset privs`)
+       }
+     break;
+     case "!bet1":
+       var die1 = random(1,7);
+       var die2 = random(1,7);
+       msg.channel.send(`Die 1 result: ${die1}\nDie 2 result: ${die2}`\n) 
+      
        
        
-       
-       
-       
+       //not done yet lol
        
     
        
