de9ed108d54ff4595b4470c01a45c89324d370d8
Patrick Allen <twistedchaos93@gmail.com>
2018-03-22 21:07:03 -0400

    initial upload of stuff

---
+++ app.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const settings = require('./settings.json')
+
+client.on("ready",() => {
+  console.log("Im ready dog");
+});
+
+var prefix = "~";
+var msgContents;
+var swears = ["fuck", "shit", "dammit", "damn", "bitch", "ech"];
+
+client.on('message', msg => {
+  const Heresy = msg.guild.emojis.find("name", "Heresy");
+  const ChristianServer = msg.guild.emojis.find("name", "ChristianServer");
+  if (msg.author === client.user) return;
+  msgContents = msg.content;
+
+
+
+  if (testList(swears)) {
+    msg.channel.send(ChristianServer.toString()+' YOU HECKIN SWORE '+ ChristianServer.toString());
+  }else if(msgContents.toLowerCase().includes("heresy")){
+    msg.channel.send('Did somebody say heresy? '+ Heresy.toString());
+
+  }
+
+});
+
+client.login(settings.token);
+
+function testList(array){
+  for(var i = 0; i<array.length; i++){
+    if (msgContents.toLowerCase().includes(array[i])){
+      return(true);
+    }
+  }
+  return(false);
+}
